a27add8f6690b4fb3ce311fecc1eafb1521e1688
petiesmo <66927015+petiesmo@users.noreply.github.com>
2022-03-17 14:28:40 -0600

    TTParser methods; new GUI - need tests

--- src/smparser.py
+++ src/smparser.py
@@ -1,13 +1,13 @@
 #! /bin/python3/smparser.py
-
+#%%
 from pathlib import Path
 from datetime import datetime
 import logging
 import sys
 
 import PySimpleGUI as sg
-from smparser_classes import SMParser, IGParser, FBParser, Media
-
+from smparser_classes import IGParser, FBParser
+#%%
 def resource_path(relative_path):
 	'''Discovers the temporary extract folder for the Executable,
 		for loading stored images or other data'''
@@ -17,60 +17,125 @@ def resource_path(relative_path):
 		base_path = Path.cwd()
 	return str(base_path / relative_path)
 
+def GUI():
+	def get_zip_file(sm_platform, home_path):
+		return sg.popup_get_file(f'Select {sm_platform} zip file', title='Select Zip file', 
+								default_path=f"{home_path / 'Inbox'}", default_extension='.zip', 
+								history=True, history_setting_filename=HISTORY)  
+	
+	'''Window for user inputs to launch the scrubber process'''
+	layout = [	[sg.T('Fill in details for this Candidate:')],
+				[sg.T('First Name: '), sg.push(), sg.I(key='person_first_name') ],
+				[sg.T('Last Name: '), sg.push(), sg.I(key='person_last_name')],
+				[sg.T('Alias: '), sg.push(), sg.I(key='person_alias')],
+				[sg.T('Pick last date'), sg.push(), B('End Date>>>'), sg.I(key='last_date', default=None)],
+				[sg.T('How many months back?'), sg.Slider(range=(1,120), default_value=24, key='months_back')]
+				[sg.HorizontalSeparator()],
+				[sg.T('Choose Main Directory: '), sg.push(), sg.B('HOME>>>'), sg.I(key='fp_person') ],
+				[	sg.column([[sg.Listbox(zipfiles, size=(25,8), key='-ZIPS-')]]),
+					sg.push(),
+					sg.column([	[sg.B('FB>>>'), sg.I(key='FBzip', default=None)],
+								[sg.B('IG>>>'), sg.I(key='IGzip', default=None)],
+								[sg.B('TT>>>'), sg.I(key='TTzip', default=None)],
+								[sg.B('SC>>>'), sg.I(key='SCzip', default=None)]], size=35)
+				]
+				[sg.T('')],
+				[sg.Image(LOGO), sg.push(), sg.B('OK'), sg.B('Cancel')],
+				[sg.StatusBar('Not all values filled', key='-STATUS-', justification='right')]
+			]
+
+	window = sg.Window('Candidate Info', layout=layout, finalize=True)
+
+	# Event Loop
+	while True:     
+		event, values = window.read()
+		if event in (sg.WIN_CLOSED, 'Cancel'):
+			logging.info('The User closed the data entry screen')
+			break
+		if event == 'OK':
+			window['-TEXT-KEY-'].update(values['-INPUT-'])
+		if event == 'HOME>>>':
+			fp_person = Path(sg.popup_get_folder('Select the folder for the Person.\n(Outbox folder will be created here)\n(Social Media zip files are typically here also)', 
+					title='Person Folder', history=True, history_setting_filename=HISTORY, image=LOGO))
+			window['fp_person'].update(fp_person)
+		if event == 'FB>>>':
+			window['FBzip'].update(get_zip_file('Facebook(FB)', fp_person))
+		if event == 'IG>>>':
+			window['IGzip'].update(get_zip_file('Instagram(IG)', fp_person))
+		if event == 'TT>>>':
+			window['TTzip'].update(get_zip_file('TikTok(TT)', fp_person))
+		if event == 'SC>>>':
+			window['SCzip'].update(get_zip_file('Snapchat(SC)', fp_person))
+		if event == 'End Date>>>':
+			_last_date = sg.popup_get_date(title='Choose End Date (Default is Today)', no_titlebar=False)
+			if _last_date is not None:
+				m,d,y = _last_date
+				last_date = datetime(y,m,d)
+			else:
+				last_date = datetime.today()
+			window['last_date'] = last_date.strftime("%MM-%DD-%YYYY")
+	window.close()
+	return values
+
 def parse_main():
-	#Setup GUI and Logging settings
-	tempdir = Path.home() / "AppData" / "Local" / "SMParser"
-	if not tempdir.exists(): tempdir.mkdir()
-	sg.theme('DarkBrown4')
-	us = sg.UserSettings()
-	sg.user_settings_filename(path=tempdir)
-	HISTORY = f"{tempdir / 'history.json'}"
 	#Request path for output files
 	fp_person = Path(sg.popup_get_folder('Select the folder for the Person.\n(Outbox folder will be created here)\n(Social Media zip files are typically here also)', 
 					title='Person Folder', history=True, history_setting_filename=HISTORY, image=LOGO))
 	logfile = f"{fp_person / 'parser.log'}"
 	logging.basicConfig(format="%(asctime)s|%(levelname)s:%(message)s", filename=logfile, level=logging.DEBUG) # encoding='utf-8')
 	#Request parse instance information (IDs, date range)
-	person_name = sg.popup_get_text('Enter Person name or initials', title="Person's Name", image=LOGO)
-	person_alias = sg.popup_get_text('Enter an Alias for person', title="Alias")
-	_last_time = sg.popup_get_date(title='Choose End Date (Default is Today)', no_titlebar=False)
-	if _last_time is not None:
-		m,d,y = _last_time
-		last_time = datetime(y,m,d)
+	person_first_name = sg.popup_get_text("Enter Person's first name", title="Person's First Name", image=LOGO)
+	person_last_name = sg.popup_get_text("Enter Person's last name", title="Person's Last Name", image=LOGO)
+	person_alias = sg.popup_get_text('Enter an Alias for person', title='Alias')
+	_last_date = sg.popup_get_date(title='Choose End Date (Default is Today)', no_titlebar=False)
+	if _last_date is not None:
+		m,d,y = _last_date
+		last_date = datetime(y,m,d)
 	else:
-		last_time = datetime.today()
+		last_date = datetime.today()
 	_months_back = sg.popup_get_text('How many months back?', "Months Back", '24')
-	months_back = int(_months_back) if _months_back.isnumeric() and int(_months_back) > 0 else 24
+	months_back = int(_months_back) if (_months_back.isnumeric() and int(_months_back) > 0) else 24
 	#Request paths to input data zip files
-	FBzip = sg.popup_get_file('Select Facebook(FB) zip file', title='FB Zip file', 
+	FBzip = sg.popup_get_file(	'Select Facebook(FB) zip file', title='FB Zip file', 
 								default_path=f"{fp_person / 'Inbox'}", default_extension='.zip', 
 								history=True, history_setting_filename=HISTORY)  
-	IGzip = sg.popup_get_file('Select Instagram(IG) zip file', title='IG Zip file', 
+	IGzip = sg.popup_get_file(	'Select Instagram(IG) zip file', title='IG Zip file', 
 								default_path=f"{fp_person / 'Inbox'}", default_extension='.zip', 
 								history=True, history_setting_filename=HISTORY) 
-
+	#Diagnostics
 	logging.info(f'Person: {person_name}, Alias: {person_alias}')
-	logging.info(f'Last time: {last_time}, Months Back: {months_back}')
+	logging.info(f'Last time: {last_date}, Months Back: {months_back}')
 	logging.info(f'FB File: {FBzip}')
 	logging.info(f'IG File: {IGzip}')
-	
+	#Launch Parsers
 	if FBzip is None:
 		logging.info('Skipped FB')
 	else:
-		FB = FBParser(person_name, person_alias, FBzip, home_dir=fp_person, months_back=months_back, last_time=last_time)
+		FB = FBParser(	last_name=person_last_name, first_name=person_first_name, person_alias=person_alias,
+						zip_path=FBzip, home_dir=fp_person, months_back=months_back, last_date=last_date)
 		FB.parse_FB_data()
 		logging.info('FB Parsing complete')
-		sg.popup_timed('FB Parsing complete',non_blocking=True)
+		sg.popup_timed('FB Parsing complete', non_blocking=True)
 
 	if IGzip is None:
 		logging.info('Skipped IG')
 	else:
-		IG = IGParser(person_name, person_alias, IGzip, home_dir=fp_person, months_back=months_back, last_time=last_time)
+		IG = IGParser(	last_name=person_last_name, first_name=person_first_name, person_alias=person_alias,
+						zip_path=IGzip, home_dir=fp_person, months_back=months_back, last_date=last_date)
 		IG.parse_IG_data() 
 		logging.info('IG Parsing complete')
-		sg.popup_timed('IG Parsing complete',non_blocking=True)
-	print('al fin')
+		sg.popup_timed('IG Parsing complete', non_blocking=True)
+	
 
 if __name__ == '__main__':
+	#Setup GUI and Logging settings
 	LOGO = resource_path(r'BrownU_logo.png')
+	sg.theme('DarkBrown4')
+	tempdir = Path.home() / "AppData" / "Local" / "SMParser"
+	if not tempdir.exists(): tempdir.mkdir()
+	#us = sg.UserSettings()
+	#sg.user_settings_filename(path=tempdir)
+	HISTORY = f"{tempdir / 'history.json'}"
+	
 	parse_main()
+	print('al fin')
--- src/smparser_classes2.py
+++ src/smparser_classes2.py
@@ -1,83 +1,182 @@
+#%%
+#smparser-classes2.py
 from smparser_classes import SMParser
-
+#%%
 class TTParser(SMParser):
     '''Parser class for TikTok user data'''
-    #def __init__(self, person_name, person_alias, zip_path, home_dir=None):
-    #    pass
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
     def parse_profile_metadata(self):
         logging.info('Parsing TT profile metadata')
-        data = (self.get_txt('Profile','Profile Info'))[0]
+        data = (super().get_txt('Profile','Profile Info'))[0]
         header = ['Profile Item', 'Value']
         self.username = data["Username"]
-	data['Birthdate'] = {{BIRTHDAY}}
-	payload = [{'Profile Item':k, 'Value': self.scrubber.clean(v)} for k,v in data] 
-        self.genCSV('TT_Profile', header, data)
+	    data['Birthdate'] = {{BIRTHDAY}}
+	    payload = [{'Profile Item':k, 'Value': self.scrubber.clean(v)} for k,v in data] 
+        super().genCSV('TT_Profile', header, payload)
         return None
 		
     def parse_follow(self):
-        '''Parsing followers - Aggregated Total counts'''
-        logging.info("Parsing TT Follow")
-        data = self.get_txt('Activity', 'Follower')
-        data2 = self.get_txt('Activity', 'Following')
-        follow_header = ['Followers', 'Following']
+        '''Parsing TT follow activity - Aggregated Total counts'''
+        logging.info('Parsing TT Follow')
+        header = ['Followers', 'Following']
+        #Follower.txt -> {Date, Username}
+        #Following.txt -> {Date, Username}
+        data = super().get_txt('Activity', 'Follower')
+        data2 = super().get_txt('Activity', 'Following')
+        #TODO: Parse date time / filter within date range
         payload = [
             {'Followers': len(data), 
             'Following': len(data2)}]
-        self.genCSV("TT_follow", follow_header, payload)
+        super().genCSV('TT_follow', header, payload)
         return None
 
     def parse_hashtags(self):
-        '''Parsing Hashtags - List of Hashtags with favorites noted'''
-        logging.info("Parsing TT Hashtags")
-        data = self.get_txt('Activity', 'Hashtag')
-        data2 = self.get_txt('Activity', 'Favorite HashTags')
+        '''Parsing TT Hashtags - List of Hashtags with favorites noted'''
+        logging.info('Parsing TT Hashtags')
+        header = ['Hashtag Name', 'Hashtag Link', 'Favorite']
+        #Hashtag.txt -> {Hashtag Name, Hashtag Link}
+        #Favorite HashTags.txt -> {Hashtag Name, Hashtag Link}
+        data = super().get_txt('Activity', 'Hashtag')
+        data2 = super().get_txt('Activity', 'Favorite HashTags')
+        #Note: No dates/times
         fht = [ht['Hashtag Name'] for ht in data2]
-		hashtag_header = ['Hashtag Name', 'Hashtag Link', 'Favorite']
         for ht in data:
-		ht['Favorite'] = 'Yes' if (ht['Hashtag Name'] in fht) else ''
-        self.genCSV("TT_hashtags", hashtag_header, data)
+		    ht['Favorite'] = 'Yes' if (ht['Hashtag Name'] in fht) else ''
+        super().genCSV('TT_hashtags', header, data)
         return None
+
+    def parse_user_searches(self):
+        '''Parsing TT Search activity - List of Searches by date/time'''
+        #Searches.txt -> {Date, Search Term}
+        logging.info('Parsing TT Search Activity')
+        header = ['Date', 'Search Term']
+        data = super().get_txt('Activity', 'Searches')
+        #TODO: Parse date time / filter within date range
+        super().genCSV('TT_searches', header, data)
+        return None
+
+    def parse_user_likes(self):
+        '''Parsing TT Like activity - List of Likes by date/time'''
+        #Likes.txt -> {Date, Video Link}
+        logging.info('Parsing TT Search Activity')
+        header = ['Date', 'Video Link']
+        data = super().get_txt('Activity', 'Likes')
+        #TODO: Parse date time / filter within date range
+        super().genCSV('TT_likes', header, data)
+        return None
+
+    def parse_video_browsing(self):
+        '''Parsing TT video browsing activity'''
+        logging.info('Parsing TT video Activity')
+        #Video Browsing.txt -> {Date, Video Link}
+        #Favorite Videos.txt -> {Date, Video Link}
+        header = ['Date', 'Video Link', 'Favorite']
+        data = super().get_txt('Activity', 'Video Browsing')
+        data2 = super().get_txt('Activity', 'Favorite Videos')
+        data3 = super().get_txt('Activity', 'Likes')
+        #TODO: Parse date time / filter within date range
+        fv = [vid['Video Link'] for vid in data2]
+        lk = [vid['Video Link'] for vid in data3]
+        for vid in data:
+		    vid['Favorite'] = 'Yes' if (vid['Video Link'] in fv) else ''
+            vid['Liked'] = 'Yes' if (vid['Video Link'] in lk) else ''
+        super().genCSV('TT_video_browing', header, data)
+        return None
+
+    def parse_comments_from_others(self):
+        '''Parsing TT Comments from others'''
+        # Comments.txt -> {Date, Comment}
+        logging.info('Parsing TT Comments from others')
+        header = ['Date', 'Comment']
+        data = super().get_txt('Comments', 'Comments')
+        #TODO: Parse date time / filter within date range
+        #TODO: Scrub comment text
+        super().genCSV('TT_searches', header, data)
+        return None
+
+    def parse_user_videos(self):
+        '''Parsing TT Videos posted by User'''
+        # Videos.txt -> {Date, Video Link, Like(s)}
+        logging.info('Parsing TT Videos posted by user')
+        header = ['Date', 'Video Link', 'Like(s)']
+        data = super().get_txt('Videos', 'Videos')
+        #TODO: Parse date time / filter within date range
+        super().genCSV('TT_videos', header, data)
+        return None
+    
+    def parse_TT_data(self):
+		self.parse_profile_metadata()
+		self.parse_follow()
+		self.parse_hashtags()
+		self.parse_user_searches()
+        self.parse_user_likes()
+        self.parse_video_browsing()
+        self.parse_comments_from_others()
+        self.parse_user_videos()
+		# Note: No 'media' to scrub
     '''
     App Settings\Block List.txt
-    Activity\Favorite HashTags.txt
-    Activity\Hashtag.txt
-    Activity\Follower.txt
-    Activity\Following.txt
-    Activity\Likes.txt
-    Comments\Comments.txt
-    Videos\Videos.txt
-    Profile\Profile Info.txt
-    Activity\Searches.txt
-    Activity\Favorite Videos.txt''' 
+  X  Activity\Favorite HashTags.txt
+  X  Activity\Hashtag.txt
+  X  Activity\Follower.txt
+  X  Activity\Following.txt
+  X  Activity\Likes.txt
+  X  Comments\Comments.txt
+  X  Videos\Videos.txt
+  X  Profile\Profile Info.txt
+  X  Activity\Searches.txt
+  X  Activity\Favorite Videos.txt''' 
 
     '''TikTok
-    participant's profile = Profile --> Profile Info
-    comments that others have posted on participants posts = Comments
-    most used hashtags = Activity --> Favorite HashTags
-    number of followers/following/blocked users =  Activity --> Follower, Following
-    search history =  Activity --> Searches
-    most watched videos = Activity --> Favorite Videos
-    time stamps from when videos were posted or browsed = ??
-    number of likes on participant's posts =  Activity --> Likes
+  X  participant's profile = Profile --> Profile Info
+  X  comments that others have posted on participants posts = Comments
+  X  most used hashtags = Activity --> Favorite HashTags
+  X  number of followers/following/blocked users =  Activity --> Follower, Following
+  X  search history =  Activity --> Searches
+  X  most watched videos = Activity --> Favorite Videos
+  X  time stamps from when videos were posted or browsed = ??
+  X  number of likes on participant's posts =  Activity --> Likes
     '''
-
+#%%
 class SCParser(SMParser):
     '''Parser class for SnapChat data'''
-    #def __init__(self, person_name, person_alias, zip_path, home_dir=None):
-    #    pass 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def parse_profile_metadata(self):
+        logging.info('Parsing SC profile metadata')
+        data = super().get_json('','user_profile')
+        '''header = ['Profile Item', 'Value']
+        self.username = data["Username"]
+	    data['Birthdate'] = {{BIRTHDAY}}
+	    payload = [{'Profile Item':k, 'Value': self.scrubber.clean(v)} for k,v in data] 
+        '''super().genCSV('SC_Profile', header, payload)
+        return None
+
+    def parse_follow(self):
+		'''Parsing SC followers - Aggregated Total counts'''
+		logging.info("Parsing SC Follow")
+		data = super().get_json('', 'friends')
+		#data2 = super().get_json('followers_and_following', 'following')
+		header = ['Friends', 'Blocked', 'Pending']
+		payload = [ {'Friends': len(data.friends)},
+                    {'Blocked': len(data.blocked)},
+                    {'Pending': len(data.pending)}] 
+		super().genCSV("SC_follow", header, payload)
     '''
-    friends.json
+   X friends.json
     ranking.json
     story_history.json
     talk_history.json
-    user_profile.json
+   X user_profile.json
     ?Public profile?'''
 
     '''Snapchat: 
 
     time spent on app = ??
     type of content participant is interacting with = ranking.json OR user_profile.json under "Discover Channels Viewed" and "Interest Categories" OR subscriptions.json "
-    number of followers = friends.json
+   X number of followers = friends.json
     number of views on participant's posts = story_history.json
-    friend requests sent, deleted users and blocked users, and ignored snapchatters by participant = ??'''
+   X friend requests sent, deleted users and blocked users, and ignored snapchatters by participant = ??'''
